1b0de9b64b0144101bb4bdd989c1f030a136b791
John Kelly <jkelly3rd@gmail.com>
2024-07-24 13:29:27 -0400

    Update map tile and color styling for perimeters

--- scripts/wildfires_scrape_build.R
+++ scripts/wildfires_scrape_build.R
@@ -303,9 +303,9 @@ wildfire_map <- leaflet(nfis_perimeters, options = leafletOptions(zoomControl = 
   addControl(position = "topleft", html = headerhtml, className="map-title") %>%
   setView(-115, 40, zoom = 5) %>%
   #  addProviderTiles(providers$CartoDB.Positron) %>%
-  addProviderTiles(providers$OpenStreetMap) %>%
+  addProviderTiles(providers$Esri.WorldStreetMap) %>%
   addPolygons(data = nfis_perimeters, 
-              color = "#FD8724",
+              color = "red",
               popup = perimeterLabel,
               weight = 1.5) %>%
   addAwesomeMarkers(data = fires,
