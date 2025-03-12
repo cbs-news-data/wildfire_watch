7265dc272f33455cc60b203daa031ec83448ff63
johnstont05 <taylorfjohnston@gmail.com>
2025-03-12 10:13:25 -0400

    reverting

--- scripts/page_datetime.R
+++ scripts/page_datetime.R
@@ -1,19 +1,15 @@
-# Load necessary library
+# Libraries
 library(stringr)
 
-# Get current time in UTC and adjust for Pacific Standard Time (always PST, not PDT)
-updated_datetime <- format(as.POSIXct(Sys.time(), tz = "UTC"), 
-                           "%B %d, %Y at %I:%M %p")
+# Get updated date/time in "America/Los_Angeles" timezone and format it
+updated_datetime <- format(Sys.time(), "%B %d, %Y at %I:%M %p %Z", tz = "America/Los_Angeles")
 
-# Replace " 0" with a space for formatting
+# Replace " 0" with a space to clean up the format
 updated_datetime_pretty <- str_replace_all(updated_datetime, " 0", " ")
 
 # Replace AM/PM with a.m./p.m.
 updated_datetime_pretty <- str_replace_all(updated_datetime_pretty, "AM", "a.m.")
 updated_datetime_pretty <- str_replace_all(updated_datetime_pretty, "PM", "p.m.")
-
-# Append PST manually
-updated_datetime_pretty <- paste(updated_datetime_pretty, "PST")
 
 # Save to CSV
 write.csv(data.frame(updated_datetime_pretty), "data/updated_datetime_df.csv", row.names = FALSE)
